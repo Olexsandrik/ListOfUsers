ec64b062264aed7e39673147a9cf3736df046c65
Olexsandrik <119703838+Olexsandrik@users.noreply.github.com>
2024-07-23 19:35:53 +0300

    updateAllLogic

--- src/components/pages/Posts.jsx
+++ src/components/pages/Posts.jsx
@@ -10,6 +10,9 @@ import {Loader} from '../Loading/Loader';
 import {useFetching} from '../hooks/useFetching';
 import {getPagesCount} from '../../utils/pages'
 import { Paginations } from '../UI/Paginations/Pagination';
+import { useRef } from 'react';
+import { useObserver } from '../hooks/useObserver';
+import { MySelect } from '../UI/select/MySelect';
 
 export function Posts() {
  
@@ -20,21 +23,30 @@ export function Posts() {
   const [totalPages,setTotalPages] = useState(0);
   const [limit,setLimit] = useState(10);
   const [page,setPage] = useState(1);
+  const lastElement = useRef();
 
+ 
 
   const [fetchPosts, isPostsLoading, postError] = useFetching( async()=>{
     const response = await PostService.getAll(limit,page);
 
-    setPosts(response.data);
+    setPosts([...posts,...response.data]);
     const totalCount = response.headers['x-total-count']; 
     setTotalPages(getPagesCount(totalCount,limit));
   })
  
 
 
+
+
+
+
+  useObserver(lastElement, page<totalPages, isPostsLoading, ()=>{
+    setPage(page+1);
+  })
   useEffect(()=>{
     fetchPosts();
-  },[page])
+  },[page,limit])
 
   const createPost=(newPost)=>{
       setPosts([...posts, newPost]);
@@ -66,14 +78,28 @@ export function Posts() {
       </MyModal>
       <hr style={{marginTop:'15px'}} />
     <PostFilter filter={filter} setFilter={setFilter}/>
-   
+
+      <MySelect
+      value={limit}
+      onChange={e=> setLimit(e)}
+
+      defaulvalue={'кількість елементів на сторінці'}
+      option={[
+        {value: 5, name: '5'},
+        {value: 10, name: '10'},
+        {value: 25, name: '25'},
+        {value: -1, name: 'ALL POSTS'}
+      ]}
+      />
    
         {postError &&
           <h1> Винекла помилка ${postError}</h1>
         }
 
         {isPostsLoading ? <div style={{display:'flex', justifyContent: "center", marginTop:'20px'}}><Loader/></div> : <PostList remove={removePost} posts={sortedAndSearchedPost} title={"Перший пост"}/>}
+        <div ref={lastElement} style={{height:'20px', background:'red'}}/>
         <Paginations totalPages={totalPages} page={page} changePage={changePage}/>
+       
         
     </div>
     
